3671a37bdeb5d8e3ff50be5b1636c3f3a52b427f
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-06-03 21:13:16 -0400

    Added 2 more papers.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 let scientificPapers = [
     //Subject must be [], with the array accepting strings or else it'll break functionality in the app.get request below.
     //Id must be integer for accepting delete requests.
+    //Author only lists first author but should be modified later.
     {
         name: 'A Decade of Microaggression Research and LGBTQ Communities: An Introduction to the Special Issue',
         id: 1,
@@ -18,6 +19,25 @@ let scientificPapers = [
         publishicationDate: "November 07, 2018",
         author: "Kevin L. Nadal",
     },
+    {
+        name: 'Dietary sources, health benefits, and risks of caffeine',
+        id: 2,
+        abstract: 'Dietary intake of caffeine has significantly increased in recent years, and beneficial and harmful effects of caffeine have been extensively studied. This paper reviews antioxidant and anti-inflammatory activities of caffeine as well as its protective effects on cardiovascular diseases, obesity, diabetes mellitus, cancers, and neurodegenerative and liver diseases. In addition, we summarize the side effects of long-term or excessive caffeine consumption on sleep, migraine, intraocular pressure, pregnant women, children, and adolescents. The health benefits of caffeine depend on the amount of caffeine intake and the physical condition of consumers. Moderate intake of caffeine helps to prevent and modulate several diseases. However, the long-term or over-consumption of caffeine can lead to addiction, insomnia, migraine, and other side effects. In addition, children, adolescents, pregnant women, and people who are sensitive to caffeine should be recommended to restrict/reduce their intake to avoid potential adverse effects.',
+        link: 'https://www.tandfonline.com/doi/full/10.1080/10408398.2022.2074362',
+        subject: ['Caffeine', 'cancer', 'cardiovascular disease', 'diabetes mellitus', 'liver disease', 'neurodegenerative disease', 'obesity', 'side effect'],
+        publishicationDate: "May 16, 2022",
+        author: 'Adila Saimaitia',
+    },
+    {
+        name: 'Is caffeine mouth rinsing an effective strategy to improve physical and cognitive performance? A systematic review',
+        id: 3,
+        abstract: 'The aim of this study was to perform a systematic review on the effects of caffeine mouth rinsing on physical and cognitive performance. Following a search through 4 databases, 18 studies were found meeting the inclusion criteria (15 for physical performance and 3 for cognitive performance). All selected studies found an improvement in cognitive performance with caffeine mouth rinse. Four studies found positive effects of caffeine mouthwash on physical performance when repeated during exercise, while one study detected a positive effect with a single mouthwash before exercise, but only in a fasted state. Among these studies that showed positive effects, however, three (2 for physical performance and 1 for cognitive performance) presented fair methodological quality. There was also a variety of methodological approaches in the studies that showed no improvement in physical performance with caffeine mouth rinse, which may have influenced the potential to detect the ergogenic effect of caffeine mouth rinse. Thus, the effects of caffeine mouth rinse on physical performance are mixed, but a potential ergogenic effect might be present in a fasted state and when mouthwash is repeated during exercise. Concerning cognitive performance, caffeine mouth rinse seems to be a beneficial strategy.',
+        link: 'https://www.tandfonline.com/doi/full/10.1080/10408398.2021.1949576',
+        subject: ['Bitter tastant', 'cognition', 'ergogenic aids', 'exercise', 'sports nutrition'],
+        publishicationDate: 'July 19, 2021',
+        author: 'Widemar Ferraz da Silva',
+    },
+
     //Default
     {
         name: null,
@@ -26,6 +46,7 @@ let scientificPapers = [
         link: null,
         subject: [],
         publishicationDate: null,
+        author: null,
     },
 ]
 
